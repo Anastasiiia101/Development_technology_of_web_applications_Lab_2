6186b1ec2c7a7c65b0b291c1fba28b8e7bbc8db5
Anastasiiia101 <anastasiia.korkishko@gmail.com>
2021-05-30 23:21:27 +0300

    Development technology of web applications Lab 2

---
+++ shortener/urls.py
@@ -0,0 +1,15 @@
+# api/urls.py
+from django.urls import path
+from functools import partial
+
+from . import views
+
+urlpatterns = [
+    path('', views.URLList.as_view(), name='url-list'),
+    path('<int:pk>/', views.URLDetail.as_view(), name='url-detail'),
+    path('', views.UserList.as_view()),
+    path('<int:pk>/', views.UserDetail.as_view(), name='user-details'),
+    path('profile/', views.Profile.as_view(), name='user-profile'),
+    path('communication/<str:room_name>/', views.Communication.as_view(), name='communication'),
+    path('users/status/', views.StatusUserList.as_view(), name='user-status'),
+]
